58721cb6ea11fdf1f2797ca1c9dbdc77d8f66d9d
wadjidali <wadjidali7@gmail.com>
2026-08-19 12:46:56 +0000

    fix: update prediction pipeline logic

--- app.py
+++ app.py
@@ -54,17 +54,22 @@ def home():
 
 @app.post("/predict")
 def predict(request: TextRequest):
-  if model is None:
-    raise HTTPException(
-        status_code=500, detail="Modèle non chargé dans le conteneur."
-    )
+    if model is None:
+        raise HTTPException(
+            status_code=500, detail="Modèle non chargé dans le conteneur."
+        )
 
-  try:
-    preds = model.predict([request.text])
-    pred_val = int(preds[0])
-    return {
-        "text": request.text,
-        "prediction": pred_val,
-        "sentiment": "Positif" if pred_val == 0 else "Négatif",    }
-  except Exception as e:
-    raise HTTPException(status_code=500, detail=str(e))
+    try:
+        # Passer le texte directement au pipeline
+        preds = model.predict([request.text])
+        pred_val = int(preds[0])
+        
+        return {
+            "text": request.text,
+            "prediction": pred_val,
+            # Vérifie selon ton encodage y dans train.py : 
+            # Si dans le dataset 1 = Positif et 0 = Négatif :
+            "sentiment": "Positif" if pred_val == 1 else "Négatif",
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
